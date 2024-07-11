d4524d5209d2bc547d101c3044a01ba7a7c0d9a9
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-07-11 12:12:48 -0300

    feat: find peak registration hours & days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -43,6 +44,29 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def find_most_repeated_elements(array)
+  counts = array.group_by { |element| element }.to_a
+  counts_sorted = counts.sort_by { |element| -element[1].length }
+
+  counts_sorted.select { |element| element[1].length > 1 }.map { |element| element[0] }
+end
+
+def find_peak_registration_hours(registration_dates)
+  registration_hours = registration_dates.map do |date|
+    date.strftime('%H').to_i
+  end
+
+  find_most_repeated_elements(registration_hours)
+end
+
+def find_peak_registration_days(registration_dates)
+  registration_days = registration_dates.map do |date|
+    date.strftime('%A')
+  end
+
+  find_most_repeated_elements(registration_days)
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -54,12 +78,15 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+registration_dates = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
+  registration_dates << Time.strptime(row[:regdate].to_s, '%m/%d/%y %H:%M')
 
   form_letter = erb_template.result(binding)
 
